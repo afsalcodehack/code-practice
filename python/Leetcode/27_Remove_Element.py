41cec8dc9b084b7a4a5b78a7e3ac0403cae2d6f5
afsal <afsal.aimit.2010@hotmail.com>
2023-08-13 21:48:09 +0300

    remove element 27

--- python/Leetcode/27_Remove_Element.py
+++ python/Leetcode/27_Remove_Element.py
@@ -9,21 +9,23 @@ class Solution:
         _count = len(nums)
         for i in range(0, len(nums)):
             if nums[i] == val:
-                _count = _count - 1
                 if nums[_len] == val:
                     nums[_len] = '_'
                     nums[i] = '_'
+                    _count = _count - 2
                 else:
                     nums[i] = nums[_len]
                     nums[_len] = '_'
-            _len = _len - 1
-        print(nums)
+                    _count = _count - 1
+                _len = _len - 1
+        print(nums , '->', _count)
         return _count
 
 
 service = Solution()
-print(service.removeElement([2, 3, 2, 6], 2))
-print(service.removeElement([2, 2, 2, 2], 2))
-print(service.removeElement([1, 2, 3, 4], 3))
-print(service.removeElement([1, 1, 1,1], 3))
-
+service.removeElement([2, 2, 2, 2], 2)
+service.removeElement([2, 3, 2, 6], 2)
+service.removeElement([1, 2, 3, 4], 3)
+service.removeElement([1, 1, 1,1], 3)
+service.removeElement([1, 2, 3,4,5,6], 4)
+service.removeElement([0,1,2,2,3,0,4,2], 2)  #[0,1,4,0,3]  4
