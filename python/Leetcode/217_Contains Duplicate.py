58e3ab5d3292979ac5b5a5d896fd4e1007528281
afsal <afsal.aimit.2010@hotmail.com>
2024-04-03 15:45:14 +0300

    leetcode 217 with set also added

--- python/Leetcode/217_Contains Duplicate.py
+++ python/Leetcode/217_Contains Duplicate.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         _map = {}
@@ -11,9 +12,19 @@ class Solution:
         return False
 
 
+    def containsDuplicateWithSet(self, nums: List[int]) -> bool:
+        _map = set()
+        for i in nums:
+            if i in _map:
+                return True
+            else:
+                _map.add(i)
+        return False
+
+
 
 driver = Solution()
-print(driver.containsDuplicate([1,2,3,1]))
-print(driver.containsDuplicate([1,2,3,4]))
-print(driver.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
-print(driver.containsDuplicate([1]))
+print(driver.containsDuplicateWithSet([1,2,3,1]))
+print(driver.containsDuplicateWithSet([1,2,3,4]))
+print(driver.containsDuplicateWithSet([1,1,1,3,3,4,3,2,4,2]))
+print(driver.containsDuplicateWithSet([1]))
